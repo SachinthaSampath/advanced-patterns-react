d921a92b9ff3a2daa0fbae0b54f10e76ca66e82b
Darius Cosden <darius@cosdensolutions.io>
2024-11-22 13:44:46 +0100

    added express

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/products")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <h1>Hello World</h1>
---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from "express";
+import productsRouter from "./features/products/router";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+// JSON middleware
+app.use(express.json());
+
+// Add access control headers
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  next();
+});
+
+// Root route
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(404);
+});
+
+// Routes
+app.use("/api/products", productsRouter);
+
+// Start server
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
