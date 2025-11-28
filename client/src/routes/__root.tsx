ff5f0a9a9b6375236d133cafce5b0c056794ab2e
SachinthaSampath <sachinsampath42@gmail.com>
2025-11-28 15:34:36 +0530

    chore: theme provider fix

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/features/shared/components/ThemeProvider";
 import { trpcQueryUtils } from "@/router";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import Navbar from "../features/shared/components/Navbar";
@@ -14,25 +15,28 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 function Root() {
   return (
     <>
-      <Toaster />
-      <div className="flex justify-center gap-8 pb-8">
-        <Navbar />
-        <div className="min-h-screen w-full max-w-2xl">
-          <header className="mb-4 border-b border-neutral-200 p-4 dark:border-neutral-800">
-            <h1 className="text-center text-xl font-bold">
-              Advanced Patterns React
-            </h1>
-            <p className="text-center text-sm text-neutral-500">
-              <b>
-                <span className="dark:text-primary-500">Cosden</span> Solutions
-              </b>
-            </p>
-          </header>
-          <div className="space-y-4 p-4">
-            <Outlet />
+      <ThemeProvider defaultTheme="dark">
+        <Toaster />
+        <div className="flex justify-center gap-8 pb-8">
+          <Navbar />
+          <div className="min-h-screen w-full max-w-2xl">
+            <header className="mb-4 border-b border-neutral-200 p-4 dark:border-neutral-800">
+              <h1 className="text-center text-xl font-bold">
+                Advanced Patterns React
+              </h1>
+              <p className="text-center text-sm text-neutral-500">
+                <b>
+                  <span className="dark:text-primary-500">Cosden</span>{" "}
+                  Solutions
+                </b>
+              </p>
+            </header>
+            <div className="space-y-4 p-4">
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
